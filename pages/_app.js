eb32bea22b8b03987b858c1e51f84e569cb8adf1
Sawankerai <keraisawan@gmail.com>
2026-07-07 22:18:00 +0530

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { CurrencyProvider } from "../context/CurrencyContext";
 import { FavoriteProvider } from "../context/FavoriteContext";
 import { OrderProvider } from "../context/OrderContext";
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
         <OrderProvider>
           <SearchProvider>
             <Component {...pageProps} />
+            <Analytics />
           </SearchProvider>
         </OrderProvider>
       </FavoriteProvider>
